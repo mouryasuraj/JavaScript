07b8813695f90c1540150e10effb909a5a0af6cb
suraj <mouryasuraj6266444@gmail.com>
2023-01-04 12:43:10 +0530

    Variables and DataTypes

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 45; 
+let accountEmail = 'iamjabbadoreo@gmail.com'
+var accountPassword = '123456';
+accountCity = "Jaipur";
+let accountState;
+// accountID = 78  not allowed
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
+
+//Prefer not to use var beacuse of issue in block scope and functional scope
---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict";  //treat all js code as latest version
+
+// alert("hello");   //we are using nodejs, not browser
+
+
+
+// DataTypes
+
+let name = "Suraj"; //String
+let age = 18; //Number
+let isLoggedIn = false; //boolean
+let bigInt = 15; //bigInt
+let name1 = null; //null
+let name2 = undefined; //undefine
+
+// Primitive data types
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefines =>
+// Symbol => unique
+
+
+// non-primitive data types
+
+// object
+
+console.log(typeof "Suraj")
+console.log(typeof null); //object
