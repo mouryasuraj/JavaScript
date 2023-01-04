1cf3e4fbf95bc1e9f666a1cdab67c6df1e955aba
suraj <mouryasuraj6266444@gmail.com>
2023-01-04 12:56:33 +0530

    DataConversion and its type

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = undefined;
+console.log(typeof score);
+
+let valueInNumber = Number(score) ;
+// let valueInNumber = Number.parseInt(score) ;
+console.log(valueInNumber);
+
+
+// "33" => 33;
+// "33abc"=> NaN
+// true=>1 and false=>0
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "df";
+let isLoggedIn = "";
+let convertBoolean = Boolean(isLoggedIn)
+console.log(convertBoolean)
+
+
+let someNumber = 33;
+let stringNUmber = String(someNumber)
+console.log(typeof stringNUmber);
